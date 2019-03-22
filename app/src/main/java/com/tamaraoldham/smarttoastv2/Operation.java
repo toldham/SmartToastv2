eb1996b507219f78441c5d6bd4803179c4591f3e
toldham <oldham@ualberta.ca>
2019-03-21 21:32:32 -0600

    Added flow from "Add Profile #" to EditProfile -> create new profile. Added handler to restart Operation if there is an ESP8266 issue.

--- app/src/main/java/com/tamaraoldham/smarttoastv2/Operation.java
+++ app/src/main/java/com/tamaraoldham/smarttoastv2/Operation.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,7 +34,13 @@ public class Operation extends AppCompatActivity {
     int seconds;
     int cookMins;
     int cookSeconds;
+    int cookMultiplier;
+    int offsetMultiplier;
+    int offset;
+    int cookTimerSeconds;
+    int offsetSeconds;
     Button AddDelay;
+    Button delete;
     int delaytime = 0;
     String delay;
     String cook;
@@ -43,10 +50,18 @@ public class Operation extends AppCompatActivity {
     String START = "START";
     long delayMillisecondsLeft;
     long cookMillisecondsLeft;
+    String delaySend;
+    boolean delayStatus = false;
+    boolean cookStatus = false;
 
     SharedPreferences sharedp1;
     SharedPreferences sharedp2;
     SharedPreferences sharedp3;
+
+    SharedPreferences.Editor editorp1;
+    SharedPreferences.Editor editorp2;
+    SharedPreferences.Editor editorp3;
+
 
     private CountDownTimer delayCountDownTimer;
     private CountDownTimer cookCountDownTimer;
@@ -62,10 +77,6 @@ public class Operation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operation);
 
-        //enable buttons as default... don't put these here... it gets mad :(
-        //AddDelay.setEnabled(true);
-        //EditProfile.setEnabled(true);
-
         //read out profile number
         Intent intentEdit = getIntent();
         profileValue = intentEdit.getStringExtra("PROFILE");
@@ -73,6 +84,11 @@ public class Operation extends AppCompatActivity {
         sharedp1 = getSharedPreferences("profile1", Context.MODE_PRIVATE);
         sharedp2 = getSharedPreferences("profile2", Context.MODE_PRIVATE);
         sharedp3 = getSharedPreferences("profile3", Context.MODE_PRIVATE);
+
+        //Create editor to edit profile 1
+        editorp1 = sharedp1.edit();
+        editorp2 = sharedp2.edit();
+        editorp3 = sharedp3.edit();
 
         DisplayProfileName = findViewById(R.id.DisplayProfileName);
         DelayTimer = findViewById(R.id.DelayTimer);
@@ -86,8 +102,11 @@ public class Operation extends AppCompatActivity {
         AddDelay = findViewById(R.id.AddDelay);
         //error response for ESP8266
         response = findViewById(R.id.response);
+        //delete button
+        delete = findViewById(R.id.delete);
 
-        cookTimer = 30; //test value
+        cookMultiplier = 30; //30 seconds
+        offsetMultiplier = 15; //15 seconds
 
         //load profile contents to display, i.e. cook timer, name
         display();
@@ -120,36 +139,62 @@ public class Operation extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delete();
+                Intent intent = new Intent(Operation.this, MainMenu.class);
+                startActivity(intent);
+            }
+        });
     }
         //need to add in offset to cook time....
         public void display() {
             if (profileValue.equals("profile1")){
                 //display profile1 information
                 DisplayProfileName.setText(String.valueOf(sharedp1.getString("name", "Add Profile 1")));
-                //COOK TIME
-                //cookTimer = sharedp1.getInt("cooktime",0);
-                //set minutes/seconds
+                //COOK TIME integer value
+                cookTimer = sharedp1.getInt("cooktime",0);
+                //offset integer value
+                offset = sharedp1.getInt("offset",0);
+                //convert to seconds
+                cookTimerSeconds = cookTimer * cookMultiplier;
+                offsetSeconds = offsetMultiplier * offset;
+                cookTimerSeconds = cookTimerSeconds + offset;
             }
 
             else if (profileValue.equals("profile2")){
                 //display profile1 information
                 DisplayProfileName.setText(String.valueOf(sharedp2.getString("name", "Add Profile 2")));
-                //COOK TIME
-                //cookTimer = sharedp2.getInt("cooktime",0);
+                //COOK TIME integer value
+                cookTimer = sharedp2.getInt("cooktime",0);
+                //offset integer value
+                offset = sharedp2.getInt("offset", 0);
+                //convert to seconds
+                cookTimerSeconds = cookTimer * cookMultiplier;
+                offsetSeconds = offsetMultiplier * offset;
+                cookTimerSeconds = cookTimerSeconds + offset;
             }
             else {
                 //display profile1 information
                 DisplayProfileName.setText(String.valueOf(sharedp3.getString("name", "Add Profile 3")));
-                //COOK TIME
-                //cookTimer = sharedp3.getInt("cooktime",0);
+                //COOK TIME integer value
+                cookTimer = sharedp3.getInt("cooktime",0);
+                //offset integer value
+                offset = sharedp3.getInt("offset",0);
+                //convert to seconds
+                cookTimerSeconds = cookTimer * cookMultiplier;
+                offsetSeconds = offsetMultiplier * offset;
+                cookTimerSeconds = cookTimerSeconds + offset;
             }
         }
 
         public void setCookTimer(){
             //math to determine seconds and minutes using cook timer variable (give cook time in seconds)
             //display time on timer
-            cookMins = cookTimer / 60;
-            cookSeconds = cookTimer % 60;
+            cookMins = (cookTimerSeconds + offsetSeconds) / 60;
+            cookSeconds = (cookTimerSeconds + offsetSeconds) % 60;
             if (cookSeconds < 10){
                 cook = cookMins + ":0" + cookSeconds;
             }
@@ -178,11 +223,12 @@ public class Operation extends AppCompatActivity {
                 //disable other buttons
                 AddDelay.setEnabled(false);
                 EditProfile.setEnabled(false);
+                delete.setEnabled(false);
                 startTimers();
                 timeRunning = true;
             }
             else{
-                //send cancel information over to MCU **NEED TO ADD VOLLEY HERE
+                //send cancel information over to MCU
                 sendCancel();
                 isCancelled = true;
 
@@ -192,12 +238,14 @@ public class Operation extends AppCompatActivity {
 
         public void startTimers(){ //used to determine which timer to start, delay or cook
             if (delaytime > 0){
+                delayStatus = true;
                 sendProfile();
-                startDelay();
+                //startDelay();
             }
             else{
+                cookStatus = true;
                 sendProfile();
-                startCook();
+                //startCook();
             }
 
         }
@@ -242,7 +290,7 @@ public class Operation extends AppCompatActivity {
 
         public void startCook(){ //starts and updates cook timer
             //convert cooktime to long
-            cookMillisecondsLeft = (Long.valueOf(cookTimer)) * 1000;
+            cookMillisecondsLeft = (Long.valueOf(cookTimerSeconds)) * 1000;
             cookCountDownTimer = new CountDownTimer(cookMillisecondsLeft, 1000) {
                 @Override
                 public void onTick(long millisUntilFinished) {
@@ -281,15 +329,20 @@ public class Operation extends AppCompatActivity {
 
         public void sendProfile(){
             final RequestQueue requestQueue = Volley.newRequestQueue(Operation.this);
-            profile = server_url + "profile?profile=" + "1" + delaytime + cookTimer + "X";
+            //need to send 3 delay, cooktime number, offset number "123456X"
+            convertDelay();
+            profile = server_url + "profile?profile=" + "1" + offset + delaytime + cookTimer + "x";
 
             StringRequest stringRequest = new StringRequest(Request.Method.GET, profile, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-
+                    if (delayStatus){
+                        startDelay();
+                    }
+                    if (cookStatus) {
+                        startCook();
+                    }
                     requestQueue.stop();
-                    //startDelay(); //start operation of toaster
-
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -299,6 +352,13 @@ public class Operation extends AppCompatActivity {
                     error.printStackTrace();
                     requestQueue.stop();
                     //consider restarting activity for future...
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Operation.this.recreate();
+                        }
+                    }, 5000);
                 }
             });
             requestQueue.add(stringRequest);
@@ -322,8 +382,49 @@ public class Operation extends AppCompatActivity {
                     error.printStackTrace();
                     requestQueue.stop();
                     //consider restarting activity for future...
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Operation.this.recreate();
+                        }
+                    }, 5000);
                 }
             });
+            requestQueue.add(stringRequest);
+        }
+
+        public void convertDelay() { //convert delay to 3 digits to send to MCU
+            if (delaytime < 10) {
+                delaySend = "00" + delaytime;
+            }
+            if (delaytime < 100) {
+                delaySend = "0" + delaytime;
+            }
+        }
+
+        public void delete() {
+            switch (profileValue) {
+                case "profile1":
+                    editorp1.putString("name", "Add Profile 1");
+                    editorp1.remove("cooktime");
+                    editorp1.remove("offset");
+                    editorp1.apply();
+                    break;
+                case "profile2":
+                    editorp2.putString("name", "Add Profile 2");
+                    editorp2.remove("cooktime");
+                    editorp2.remove("offset");
+                    editorp2.apply();
+                    break;
+                case "profile3":
+                    editorp3.putString("name", "Add Profile 3");
+                    editorp3.remove("cooktime");
+                    editorp3.remove("offset");
+                    editorp3.apply();
+                    break;
+
+            }
         }
 
 
